46fb273386be8ead3f800228d491c549ebabb88c
iamatnuria <ignasiamat10@gmail.com>
2022-06-18 22:21:32 +0200

    Main logic with API made, components pending that will be made tomorrow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-
+import React, {useEffect,useState} from 'react';
 import './App.css';
+import getGifs from './services/getGifs';
 
-function App() {
+  export default function App() {
+  const [gifs, setGifs] = useState([])
+
+  useEffect(function () {
+    getGifs({ keyword: 'rick' }).then(gifs => setGifs(gifs))
+  }, [])
+
   return (
     <div className="App">
       <section className="App-content">
-
+        {
+          gifs.map(singleGif => 
+          {
+            return <div>
+              <h4>{singleGif.title}</h4>
+              <small>{singleGif.id}</small>
+              <img src={singleGif.url } alt={singleGif.title} />
+            </div>
+            })
+        }
       </section>
     </div>
   );
 }
-
-export default App;
---
+++ src/services/getGifs.js
@@ -0,0 +1,20 @@
+const apiKey = '9P5xQCqOilGUeKxn0my1OTUemA6DJ43w'
+
+export default function getGifs ({keyword = 'morty'} = {}) {
+
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+
+    return fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const {data = []} = response
+        if (Array.isArray(data)){
+          const gifs = data.map(image => {
+            const {images, title, id} = image
+            const { url } = image.images.downsized_medium
+            return { title, id, url }
+          })
+          return gifs
+        }
+      })
+}
